5eb4cb9e1226988294ba85772c411aa803714dec
Curtis Gibson <notcurtisg@gmail.com>
2021-05-23 22:22:00 -0400

    basic post list and added single page

--- posts.php
+++ posts.php
@@ -1,4 +1,21 @@
 <?php /* Template Name: Posts */ ?>
 <?php get_header(); ?>
 <h1>Posts</h1>
+
+<?php
+// wp-query to get all published posts without pagination
+$allPostsWPQuery = new WP_Query(array('post_type' => 'post', 'post_status' => 'publish', 'posts_per_page' => -1)); ?>
+<?php if ($allPostsWPQuery->have_posts()) : ?>
+
+	<div class='d-flex'>
+
+		<?php while ($allPostsWPQuery->have_posts()) : $allPostsWPQuery->the_post(); ?>
+			<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2><pre><?php the_date(); ?></pre></a>
+		<?php endwhile; ?>
+	</div>
+	<?php wp_reset_postdata(); ?>
+<?php else : ?>
+	<p><?php _e('There no posts to display.'); ?></p>
+<?php endif; ?>
+
 <?php get_footer(); ?>
