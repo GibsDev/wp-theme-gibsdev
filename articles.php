8b815d4b6403bf3f1b50d14a13d9b34aa63851dd
Curtis Gibson <notcurtisg@gmail.com>
2021-05-23 22:01:07 -0400

    added header and footer to template pages

--- articles.php
+++ articles.php
@@ -1,2 +1,4 @@
 <?php /* Template Name: Articles */ ?>
+<?php get_header(); ?>
 <h1>Articles</h1>
+<?php get_footer(); ?>
--- links.php
+++ links.php
@@ -1,2 +1,4 @@
 <?php /* Template Name: Links */ ?>
+<?php get_header(); ?>
 <h1>Links</h1>
+<?php get_footer(); ?>
